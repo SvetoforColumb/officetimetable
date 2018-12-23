5d3a72567ee1a16304f51da1c5df2429be98fd7a
Sasha Grigoryeff <a.grigoryeff@gmail.com>
2018-12-23 22:14:13 +0500

    ip print

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ bot = telebot.TeleBot(config.token)
 def handle_start_help(message):
     # dbworker.addUser(message.chat.id)
     # now = datetime.datetime.now()
-    bot.send_message(message.chat.id, "Hi! This is your time keeper bot!")
+    bot.send_message(message.chat.id, "Hi! This is your time keeper bsot!")
 
 
 # Проверим, есть ли переменная окружения Хероку (как ее добавить смотрите ниже)
