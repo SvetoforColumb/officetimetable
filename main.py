1cee05c4d2970129b7f51387b5f5cdec33660fdb
Sasha Grigoryeff <a.grigoryeff@gmail.com>
2018-12-23 23:46:53 +0500

    chaged url

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ def getMessage():
 @server.route("/")
 def webhook():
     bot.remove_webhook()
-    bot.set_webhook(url='https://your_heroku_project.com/' + config.token)
+    bot.set_webhook(url='https://officetimetable.herokuapp.com/' + config.token)
     return "!", 200
 
 
