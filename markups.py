2791e565cbd301e8b8cfcd562416d5277372568e
Sasha Grigoryeff <a.grigoryeff@gmail.com>
2018-12-27 23:52:47 +0500

    added view

--- markups.py
+++ markups.py
@@ -1,38 +1,40 @@
 from telebot import types
 import dbworker
 
+
 def getRemoveMarkup():
-        return types.ReplyKeyboardRemove()
+    return types.ReplyKeyboardRemove()
 
 
 def getMainMarkup():
-        markup_main = types.ReplyKeyboardMarkup(True)
-        markup_main.row('Make a note')
-        markup_main.row('View notes')
-        return markup_main
+    markup_main = types.ReplyKeyboardMarkup(True)
+    markup_main.row('Make a note')
+    markup_main.row('View notes')
+    return markup_main
 
 
 def getYesNoMarkup():
-        yes_no_markup = types.InlineKeyboardMarkup(row_width=2)
-        yes_no_markup_row = [
-                types.InlineKeyboardButton('No', callback_data="add_remind_no"),
-                types.InlineKeyboardButton('Yes', callback_data="add_remind_yes")]
-        yes_no_markup.row(*yes_no_markup_row)
-        return yes_no_markup
+    yes_no_markup = types.InlineKeyboardMarkup(row_width=2)
+    yes_no_markup_row = [
+        types.InlineKeyboardButton('No', callback_data="add_remind_no"),
+        types.InlineKeyboardButton('Yes', callback_data="add_remind_yes")]
+    yes_no_markup.row(*yes_no_markup_row)
+    return yes_no_markup
 
 
 def getNoteMarkup(user_id, note_id=None):
-        if note_id:
-                prev_note_id = dbworker.getPrevNoteId(note_id)[0]
-                next_note_id = dbworker.getNextNoteId(note_id)[0]
-        else:
-                note_id = dbworker.getLastNotesId(user_id)
-                prev_note_id = dbworker.getPrevNoteId(note_id)[0]
-                next_note_id = dbworker.getNextNoteId(note_id)[0]
-        note_markup = types.InlineKeyboardMarkup(row_width=2)
-        note_markup_row = [
-                types.InlineKeyboardButton('<', callback_data="prev_note-" + str(prev_note_id)),
-                types.InlineKeyboardButton('Delete', callback_data="delete_note-" + str(note_id)),
-                types.InlineKeyboardButton('>', callback_data="next_note-" + str(next_note_id))]
-        note_markup.row(*note_markup_row)
-        return note_markup
+    if note_id is None:
+            note_id = dbworker.getLastNotesId(user_id)
+            prev_note_id = dbworker.getPrevNoteId(note_id)[0]
+            next_note_id = dbworker.getNextNoteId(note_id)[0]
+
+    else:
+            prev_note_id = dbworker.getPrevNoteId(note_id)[0]
+            next_note_id = dbworker.getNextNoteId(note_id)[0]
+    note_markup = types.InlineKeyboardMarkup(row_width=2)
+    note_markup_row = [
+        types.InlineKeyboardButton('<', callback_data="prev_note-" + str(prev_note_id)),
+        types.InlineKeyboardButton('Delete', callback_data="delete_note-" + str(note_id)),
+        types.InlineKeyboardButton('>', callback_data="next_note-" + str(next_note_id))]
+    note_markup.row(*note_markup_row)
+    return note_markup
